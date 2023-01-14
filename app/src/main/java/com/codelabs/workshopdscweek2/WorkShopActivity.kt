f5729f7709ce50cad87c987868798aac5885c151
mjafarshidik <mjafarshidik86@gmail.com>
2023-01-14 14:04:08 +0700

    Initial commit

---
+++ app/src/main/java/com/codelabs/workshopdscweek2/WorkShopActivity.kt
@@ -0,0 +1,11 @@
+package com.codelabs.workshopdscweek2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkShopActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_work_shop)
+    }
+}
